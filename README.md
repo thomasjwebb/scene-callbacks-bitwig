a9f3488f50d2968af141c0bba876e339e46610b9
Thomas J. Webb <thomas@osakared.com>
2015-01-04 22:21:55 -0800

    Updated README instructions and finished initial implementation to my satisfaction.

--- README.md
+++ README.md
@@ -4,16 +4,53 @@ Scene Support for Bitwig
 Introduction
 ------------
 
-Work just began ignore this until it's actually working.
+As of when this was created, [Bitwig](http://www.bitwig.com) doesn't have callbacks in the control
+script javascript api to see if a scene is playing or being queued for playing. To allow controller
+scripts to support this until they add that support, this was created to basically fulfill that purpose.
 
-Installation
-------------
+The concept is by design very simple and the behavior isn't perfect in all cases. I didn't want to put
+too much effort into code that I hope to not need sooner rather than later. And more complex code can
+have more bugs in it anyway. They behavior is basically:
+
+* If all clips in the scene (row) in the trackview you're using that have content are playing and no other scene contains
+playing tracks, then the scene is considered to be playing
+* If all clips in the scene in the trackview you're using that have content are queued and no other scene contains queued
+tracks, then the scene is considered to be queued
+* If there are no clips with content in the scene, then the scene isn't considered to be queued or playing
+
+Sometimes, this leads to unexpected behavior. If all tracks but one are playing and you start to play that track, the
+scene won't look like it's queued. It will just go from nothing to playing once that track starts playing. Again, I
+don't feel like making this more sophisticated and I feel that's Bitwig's job but if you want to enhance, feel free
+and send me the pull request.
+
+Usage
+-----
 
 You can use this as a submodule or just manually copy the file since it's only one javascript file.
 If you use a submodule add the path before scene.js accordingly below.
-Import the code, which you will then be able to use to add the callbacks, which you can then use. 
 
     load('scene.js');
+
+Since we're not allowed by the interface to just attach arbitrary functions to objects that exist
+in Bitwig's Java, we must create a new object that has the callback register functions on it, which
+I'll call a "fake" ClipLauncherScenes object:
+
+    fakeClipLauncherScenes = addSceneStateCallbacks(main_track_bank, grid_width, grid_height);
+
+There are then two functions you can use to add observers and get feedback when this script thinks
+a scene is playing or queued.
+
+    fakeClipLauncherScenes.addIsPlayingObserver(function(scene, playing)
+    {
+        // Do something here like lighting up your scene launcher button
+    });
+
+    fakeClipLauncherScenes.addIsQueuedObserver(function(scene, queued)
+    {
+        // Do something here like making your scene launcher button blink
+    });
+
+That's all there is to it. Let me know if you find serious bugs.
 
 License
 -------
--- scene.js
+++ scene.js
@@ -1,5 +1,132 @@
+// Copyright (c) 2015, Osaka Red, LLC and Thomas J. Webb
+// All rights reserved.
 
-function prepareClip(clip, track, scenes)
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+//  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+//  in the documentation and/or other materials provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// This updates all_not_playing and all_not_queued
+function updateSceneNegativeValues(scene, num_tracks)
+{
+	all_empty = true;
+	all_not_playing = true;
+	all_not_queued = true;
+	for (i = 0; i < num_tracks; ++i)
+	{
+		clip = scene.clips[i];
+		// Doesn't have anything, skip
+		if (clip.has_content) all_empty = false;
+		else continue;
+		if (clip.playing) all_not_playing = false;
+		if (clip.queued) all_not_queued = false;
+	}
+
+	// Can't be all_playing or all_queued if all_empty
+	if (all_empty)
+	{
+		all_not_playing = all_not_queued = true;
+	}
+
+	scene.all_not_queued = all_not_queued;
+	scene.all_not_playing = all_not_playing;
+}
+
+// This updates the positive values and dispatches 
+function updateScenePositiveValues(scene, num_tracks, num_scenes, scenes, is_playing_observers, is_queued_observers)
+{
+	all_playing = true;
+	all_queued = true;
+	all_empty = true;
+	for (i = 0; i < num_tracks; ++i)
+	{
+		clip = scene.clips[i];
+		// Doesn't have anything, skip
+		if (clip.has_content) all_empty = false;
+		else continue;
+		if (!clip.playing) all_playing = false;
+		if (!clip.queued) all_queued = false;
+	}
+
+	// Can't be all_playing or all_queued if all_empty
+	if (all_empty)
+	{
+		all_playing = all_queued = false;
+	}
+
+	// Still think you're all_playing? Not if other tracks aren't stopped
+	if (all_playing)
+	{
+		for (i = 0; i < num_scenes; ++i)
+		{
+			// Skip if it's me
+			if (i == scene.index) continue;
+			if (!scenes[i].all_not_playing)
+			{
+				all_playing = false;
+				break;
+			}
+		}
+	}
+
+	// Okay, still think you're all_queued?
+	if (all_queued)
+	{
+		for (i = 0; i < num_scenes; ++i)
+		{
+			if (i == scene.index) continue;
+			if (!scenes[i].all_not_queued)
+			{
+				all_queued = false;
+				break;
+			}
+		}
+	}
+
+	// Time to update the scene variables and dispatch to listeners as need be
+	if (scene.all_playing != all_playing)
+	{
+		for (i = 0; i < is_playing_observers.length; ++i)
+		{
+			is_playing_observers[i](scene.index, all_playing);
+		}
+		scene.all_playing = all_playing;
+	}
+
+	if (scene.all_queued != all_queued)
+	{
+		for (i = 0; i < is_queued_observers.length; ++i)
+		{
+			is_queued_observers[i](scene.index, all_queued);
+		}
+		scene.all_queued = all_queued;
+	}
+}
+
+function updateScenes(scenes, num_tracks, num_scenes, is_playing_observers, is_queued_observers)
+{
+	for (j = 0; j < num_scenes; ++j)
+	{
+		scene = scenes[j];
+		updateSceneNegativeValues(scene, num_tracks);
+	}
+
+	for (j = 0; j < num_scenes; ++j)
+	{
+		scene = scenes[j];
+		updateScenePositiveValues(scene, num_tracks, num_scenes, scenes, is_playing_observers, is_queued_observers);
+	}
+}
+
+function prepareClip(clip, track, scenes, num_tracks, num_scenes, is_playing_observers, is_queued_observers)
 {
 	// The listeners are per track but we're interested in scene
 	clip_launcher = track.getClipLauncherSlots();
@@ -9,7 +136,7 @@ function prepareClip(clip, track, scenes)
 		scene = scenes[slot];
 		sub_clip = scene.clips[clip.track_index];
 		sub_clip.has_content = has_content;
-		scene.updateState();
+		updateScenes(scenes, num_tracks, num_scenes, is_playing_observers, is_queued_observers);
 	}
 	clip_launcher.addHasContentObserver(clip.content_observer);
 	
@@ -18,7 +145,7 @@ function prepareClip(clip, track, scenes)
 		scene = scenes[slot];
 		sub_clip = scene.clips[clip.track_index];
 		sub_clip.playing = playing;
-		scene.updateState();
+		updateScenes(scenes, num_tracks, num_scenes, is_playing_observers, is_queued_observers);
 	}
 	clip_launcher.addIsPlayingObserver(clip.playing_observer);
 
@@ -27,88 +154,20 @@ function prepareClip(clip, track, scenes)
 		scene = scenes[slot];
 		sub_clip = scene.clips[clip.track_index];
 		sub_clip.queued = queued;
-		scene.updateState();
+		updateScenes(scenes, num_tracks, num_scenes, is_playing_observers, is_queued_observers);
 	}
 	clip_launcher.addIsQueuedObserver(clip.queued_observer);
 }
 
-function prepareScene(scene, num_tracks)
+function prepareScene(scene, num_tracks, scenes, num_scenes)
 {
 	// All four of these indicate that all clips with content are in given state
 	// and in the case of the first two, the remainder are the latter two
 	scene.all_playing = false;
 	scene.all_queued = false;
+	scene.all_not_playing = true;
+	scene.all_not_queued = true;
 	scene.clips = [];
-
-	scene.updateState = function()
-	{
-		all_playing = true;
-		all_queued = true;
-		all_empty = true;
-		for (i = 0; i < num_tracks; ++i)
-		{
-			clip = scene.clips[i];
-			// Doesn't have anything, skip
-			if (clip.has_content) all_empty = false;
-			else continue;
-			if (!clip.playing) all_playing = false;
-			if (!clip.queued) all_queued = false;
-		}
-
-		// Can't be all_playing or all_queued if all_empty
-		if (all_empty)
-		{
-			all_playing = all_queued = false;
-		}
-
-		// Still think you're all_playing? Not if other tracks aren't stopped
-		// if (all_playing)
-		// {
-		// 	for (i = 0; i < num_scenes; ++i)
-		// 	{
-		// 		// Skip if it's me
-		// 		if (i == scene_index) continue;
-		// 		if (!scenes[i].all_stopped)
-		// 		{
-		// 			all_playing = false;
-		// 			break;
-		// 		}
-		// 	}
-		// }
-
-		// // Okay, still think you're all_queued?
-		// if (all_queued)
-		// {
-		// 	for (i = 0; i < num_scenes; ++i)
-		// 	{
-		// 		if (i == scene_index) continue;
-		// 		if (!scenes[i].all_not_queued)
-		// 		{
-		// 			all_queued = false;
-		// 			break;
-		// 		}
-		// 	}
-		// }
-
-		// Time to update the scene variables and dispatch to listeners as need be
-		if (scene.all_playing != all_playing)
-		{
-			for (i = 0; i < is_playing_observers.length; ++i)
-			{
-				is_playing_observers[i](scene.index, all_playing);
-			}
-			scene.all_playing = all_playing;
-		}
-
-		if (scene.all_queued != all_queued)
-		{
-			for (i = 0; i < is_queued_observers.length; ++i)
-			{
-				is_queued_observers[i](scene.index, all_queued);
-			}
-			scene.all_queued = all_queued;
-		}
-	}
 }
 
 // You give this function a track bank, it will listen to changes on that track bank
@@ -134,7 +193,7 @@ function addSceneStateCallbacks(track_bank, num_tracks, num_scenes)
 			{
 				scene = scenes[scene_index] = {};
 				scene.index = scene_index;
-				prepareScene(scene, num_tracks);
+				prepareScene(scene, num_tracks, scenes, num_scenes);
 			}
 			scene = scenes[scene_index];
 			clip = scene.clips[track_index] = {};
@@ -147,7 +206,7 @@ function addSceneStateCallbacks(track_bank, num_tracks, num_scenes)
 
 			if (scene_index == 0)
 			{
-				prepareClip(clip, track, scenes);
+				prepareClip(clip, track, scenes, num_tracks, num_scenes, is_playing_observers, is_queued_observers);
 			}
 		}
 	}
@@ -161,10 +220,10 @@ function addSceneStateCallbacks(track_bank, num_tracks, num_scenes)
 	{
 		is_playing_observers.push(callable);
 
-		for (scene_index = 0; scene_index < num_scenes; ++scene_index)
+		for (i = 0; i < num_scenes; ++i)
 		{
-			scene = scenes[scene_index];
-			callable(scene_index, scene.all_playing);
+			scene = scenes[i];
+			callable(i, scene.all_playing);
 		}
 	}
 
@@ -172,10 +231,10 @@ function addSceneStateCallbacks(track_bank, num_tracks, num_scenes)
 	{
 		is_queued_observers.push(callable);
 
-		for (scene_index = 0; scene_index < num_scenes; ++scene_index)
+		for (i = 0; i < num_scenes; ++i)
 		{
-			scene = scenes[scene_index];
-			callable(scene_index, scene.all_queued);
+			scene = scenes[i];
+			callable(i, scene.all_queued);
 		}
 	}
 
